025b81506bea2f36c140e328c936419bd5c6d228
Nikita <68853033+Xplous@users.noreply.github.com>
2023-02-18 01:51:40 +0300

    feat: create Header

---
+++ COP/src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import './Header.css'
+import Logo from '../../assets/svg/logo.svg'
+const Header = () => {
+
+  return (
+    <div className="header">
+     <div className='headup'>
+      <img className='logo' src={Logo} weight="122px" height="60px"/>
+      <div className='headertext_d'>
+        <h1 className='headertext'>Студенческое Общество Разработчиков</h1>
+      </div>
+     </div>
+     <div className='headdown'>
+      <div className='navbar'>
+        <div class='nav1'><h1>услуги</h1></div>
+        <div class='nav2'><h1>о нас</h1></div>
+        <div class='nav3'><h1>наши работы</h1></div>
+        <div class='nav4'><h1>контакты</h1></div>
+        <div class='nav5'><h1>профиль</h1></div>
+      </div>
+     </div>
+    </div>
+  )
+}
+
+export default Header
